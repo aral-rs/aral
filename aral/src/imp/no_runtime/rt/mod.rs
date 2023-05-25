d4ce599c66539ba46cb91c252bf88b00b812e238
jmjoy <jmjoy@apache.org>
2023-05-25 09:28:27 +0800

    JoinHandle: impl Future add cancel method.

--- aral/src/imp/no_runtime/rt/mod.rs
+++ aral/src/imp/no_runtime/rt/mod.rs
@@ -12,7 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::{future::Future, marker::PhantomData};
+use std::{
+    any::Any,
+    future::Future,
+    marker::PhantomData,
+    pin::Pin,
+    result,
+    task::{Context, Poll},
+};
 
 pub(crate) struct Builder;
 
@@ -35,6 +42,22 @@ impl Runtime {
 }
 
 pub struct JoinHandle<T>(PhantomData<T>);
+
+impl<T> Unpin for JoinHandle<T> {}
+
+impl<T> JoinHandle<T> {
+    pub async fn cancel(self) -> Option<T> {
+        no_runtime_specified!();
+    }
+}
+
+impl<T> Future for JoinHandle<T> {
+    type Output = result::Result<T, Box<dyn Any + Send + 'static>>;
+
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        no_runtime_specified!();
+    }
+}
 
 #[inline]
 pub fn spawn<T: Send + 'static>(
