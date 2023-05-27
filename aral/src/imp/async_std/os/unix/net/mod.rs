ac3e0fa4329fd6c7c712b4a399a4b88c9e344f4d
jmjoy <jmjoy@apache.org>
2023-05-27 18:58:08 +0800

    Add UnixStream and UnixListener. (#10)

--- aral/src/imp/async_std/os/unix/net/mod.rs
+++ aral/src/imp/async_std/os/unix/net/mod.rs
@@ -12,8 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use async_std::os::unix::net::UnixDatagram as AsyncStdUnixDatagram;
+use crate::io::{Read, Write};
+use async_std::{
+    io::{ReadExt, WriteExt},
+    os::unix::net::{
+        UnixDatagram as AsyncStdUnixDatagram, UnixListener as AsyncStdUnixListener,
+        UnixStream as AsyncStdUnixStream,
+    },
+};
 use std::{io::Result, net::Shutdown, path::Path};
+
+pub struct SocketAddr(async_std::os::unix::net::SocketAddr);
+
+impl SocketAddr {
+    #[inline]
+    pub fn as_pathname(&self) -> Option<&Path> {
+        self.0.as_pathname()
+    }
+
+    #[inline]
+    pub fn is_unnamed(&self) -> bool {
+        self.0.is_unnamed()
+    }
+}
 
 pub struct UnixDatagram(AsyncStdUnixDatagram);
 
@@ -29,14 +50,31 @@ impl UnixDatagram {
         self.0.connect(path.as_ref()).await
     }
 
+    #[inline]
+    pub fn local_addr(&self) -> Result<SocketAddr> {
+        self.0.local_addr().map(SocketAddr)
+    }
+
     pub fn pair() -> Result<(UnixDatagram, UnixDatagram)> {
-        let (a, b) = AsyncStdUnixDatagram::pair()?;
-        Ok((UnixDatagram(a), UnixDatagram(b)))
+        AsyncStdUnixDatagram::pair().map(|(a, b)| (UnixDatagram(a), UnixDatagram(b)))
+    }
+
+    #[inline]
+    pub fn peer_addr(&self) -> Result<SocketAddr> {
+        self.0.peer_addr().map(SocketAddr)
     }
 
     #[inline]
     pub async fn recv(&self, buf: &mut [u8]) -> Result<usize> {
         self.0.recv(buf).await
+    }
+
+    #[inline]
+    pub async fn recv_from(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
+        self.0
+            .recv_from(buf)
+            .await
+            .map(|(n, addr)| (n, SocketAddr(addr)))
     }
 
     #[inline]
@@ -57,5 +95,64 @@ impl UnixDatagram {
     #[inline]
     pub fn unbound() -> Result<UnixDatagram> {
         AsyncStdUnixDatagram::unbound().map(UnixDatagram)
+    }
+}
+
+pub struct UnixStream(AsyncStdUnixStream);
+
+impl UnixStream {
+    pub async fn connect(path: impl AsRef<Path>) -> Result<UnixStream> {
+        AsyncStdUnixStream::connect(path.as_ref())
+            .await
+            .map(UnixStream)
+    }
+
+    pub fn local_addr(&self) -> Result<SocketAddr> {
+        self.0.local_addr().map(SocketAddr)
+    }
+
+    pub fn pair() -> Result<(UnixStream, UnixStream)> {
+        AsyncStdUnixStream::pair().map(|(a, b)| (UnixStream(a), UnixStream(b)))
+    }
+
+    pub fn peer_addr(&self) -> Result<SocketAddr> {
+        self.0.peer_addr().map(SocketAddr)
+    }
+}
+
+impl Read for UnixStream {
+    async fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        ReadExt::read(&mut self.0, buf).await
+    }
+}
+
+impl Write for UnixStream {
+    async fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        WriteExt::write(&mut self.0, buf).await
+    }
+
+    async fn flush(&mut self) -> Result<()> {
+        WriteExt::flush(&mut self.0).await
+    }
+}
+
+pub struct UnixListener(AsyncStdUnixListener);
+
+impl UnixListener {
+    pub async fn accept(&self) -> Result<(UnixStream, SocketAddr)> {
+        self.0
+            .accept()
+            .await
+            .map(|(stream, addr)| (UnixStream(stream), SocketAddr(addr)))
+    }
+
+    pub async fn bind(path: impl AsRef<Path>) -> Result<UnixListener> {
+        AsyncStdUnixListener::bind(path.as_ref())
+            .await
+            .map(UnixListener)
+    }
+
+    pub fn local_addr(&self) -> Result<SocketAddr> {
+        self.0.local_addr().map(SocketAddr)
     }
 }
